7948c0a1d06dcad69fcd219dc6adfa339777f188
isjcode <98400721+isjcode@users.noreply.github.com>
2022-03-03 09:58:27 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Book book = new Book("Salam", "Ismayil J", 32, "Fiction");
+            ILibraryManager library = new ILibraryManager();
+            library.Add(book);
+            foreach (Book b in library.Filter("dsa", "Fiction"))
+            {
+               Console.WriteLine(b); 
+            }
+        }
+    }
+}
